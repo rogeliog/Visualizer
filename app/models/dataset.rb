fed6d214c987c928ed3e95135e905a6b76deeec5
Rogelio Guzman <chinog9@gmail.com>
2011-12-04 02:46:07 -0600

    Cross matching fully functional

--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -10,13 +10,17 @@ class Dataset
   validates_presence_of :name, :state_column_name, :default_column_name, :processing
 
 
-  def add matcher_1, matcher_2, name=''
-    name ||= "#{matcher_1}/#{matcher_2}"
-    self.content.each do |element|
-      element['properties']["#{name}"] = (matcher_1.to_f/matcher_2.to_f).to_s
+  def add matcher_1, matcher_2, name = nil
+    name = "#{matcher_1}/#{matcher_2}" if name.blank?
+    deep_copy = self.content.deep_copy
+
+    deep_copy.each do |element|
+      element['properties'].merge!("#{name}" => ((element['properties'][matcher_1].to_f/element['properties'][matcher_2].to_f).round(3) rescue 0)) 
     end
+    self.content = deep_copy
+    self.save!
     self.column_names << name
-    self.save
+    self.save!
   end
 
   def max
@@ -30,3 +34,10 @@ class Dataset
   end
 
 end
+
+class Object
+  def deep_copy
+    Marshal.load(Marshal.dump(self))
+  end
+end
+
