1b13c1ec9fb5570162d818b54b81e26163c210ee
Rogelio Guzman <chinog9@gmail.com>
2011-12-08 16:01:51 -0600

    Added geocode blank states methods

--- app/models/state.rb
+++ app/models/state.rb
@@ -3,4 +3,13 @@ class State
 
   field :address
   field :coords
+
+  def self.geocode_blanks
+    self.all.each do |state|
+      if state['coords'].include? nil
+        state['coords'] = Geocoder.coordinates("#{state.address}, Mexico").reverse
+        state.save
+      end
+    end
+  end
 end
